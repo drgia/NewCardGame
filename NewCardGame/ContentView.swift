1795d4bead74987d7a0727687d9c12ba6b3183ca
drgia <yugioh9011@softbank.ne.jp>
2024-08-17 02:35:59 +0900

    Initial Commit

---
+++ NewCardGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewCardGame
+//
+//  Created by 浅井翔也 on 2024/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewCardGame/NewCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewCardGameApp.swift
+//  NewCardGame
+//
+//  Created by 浅井翔也 on 2024/08/17.
+//
+
+import SwiftUI
+
+@main
+struct NewCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
